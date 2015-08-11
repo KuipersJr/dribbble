7d6f5e4e9b963d09bc3b953c44938c221cdef7b0
Vítor Avelino <avelino.vitor@gmail.com>
2015-08-10 22:44:26 -0300

    Remoção de pastas não utilizadas.

---
+++ Dribbble.Dominio/Pagina.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Dribbble.Dominio
+{
+    public class Pagina
+    {
+        public string page { get; set; }
+        public int per_page { get; set; }
+        public int pages { get; set; }
+        public int total { get; set; }
+        public List<Shot> shots { get; set; }
+    }
+}
