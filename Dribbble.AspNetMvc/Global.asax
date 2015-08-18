a19dec4888886a36d5e26bce3f1ba477051a21d3
Vítor Avelino <avelino.vitor@gmail.com>
2015-08-17 23:18:21 -0300

    Dribbble.AspNetMvc

---
+++ Dribbble.AspNetMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dribbble.AspNetMvc.MvcApplication" Language="C#" %>
---
+++ Dribbble.AspNetMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dribbble.AspNetMvc.Startup))]
+namespace Dribbble.AspNetMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
